145929ce14e60d85b204e575d7b1e7928bae6f60
Midstackdev <midstackdev@gmail.com>
2020-08-20 13:11:42 +0000

    Update series parts

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UpdateSeriesFormRequest;
 use App\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class SeriesController extends Controller
 {
@@ -15,8 +16,17 @@ class SeriesController extends Controller
         return view('series.edit', compact('series'));
     }
 
-    public function update(UpdateSeriesFormRequest $request)
+    public function update(UpdateSeriesFormRequest $request, Series $series)
     {
-        
+        $series->title = $request->title;
+        $series->save();
+
+        // dd($request->parts);
+
+        $series->parts->each(function ($parts, $index) use ($request) {
+            $parts->timestamps = false;
+            $parts->update(Arr::only($request->parts[$index], ['title', 'sort_order']));
+            // dd(Arr::only($request->parts[$index], ['title', 'sort_order']));
+        });
     }
 }
